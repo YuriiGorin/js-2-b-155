339e02b756e5f6156c60787a90a31a93d779611f
iTeacher <test@test.ru>
2020-02-02 17:08:07 +0300

    добавлены примеры 6 занятия

---
+++ lesson-06/src/data.js
@@ -0,0 +1,11 @@
+export const user = {
+    name: "John",
+    age: 25,
+    location: [35.1643, 41.1985],
+    address: {
+        city: "Spb",
+        street: "Pravdy st."
+    },
+    email: null,
+    isActive: false,
+};
